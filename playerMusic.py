bc46de6a3e38e69a99a76d8d26a44aa95540a95b
raphael-malet <115145341+raphael-malet@users.noreply.github.com>
2023-01-30 16:48:12 +0100

    push playermusic
    ajout barre de progression + temps de la musique

--- playerMusic.py
+++ playerMusic.py
@@ -2,9 +2,13 @@ from tkinter import *
 from tkinter import filedialog
 import pygame
 import time
+from mutagen.mp3 import MP3
+import tkinter.ttk as ttk
 
 # variable global
 en_pause = False
+stoper = False
+musique_longeur = ''
 
 
 # -----------
@@ -55,6 +59,9 @@ def supprimer_toutes_les_musiques():
 # ----------------
 # fonction lancer une musique
 def lancer_musique():
+    global musique_longeur
+    global stoper
+    stoper = False
     # on prend la musique selectionner dans la liste box
     musique = fenetre_liste_musique.get(ACTIVE)
     # on rajoute le chemin du fichier pour que pygame puisse lancer la musique
@@ -66,13 +73,26 @@ def lancer_musique():
     # fonction qui affiche depuis combien de temps la musique est joué
     temps_musique()
 
+    # update slider to position
+    # slider_position = int(musique_longeur)
+    # barre_progression.config(to=slider_position, value=0)
+
 
 # fonction qui permet de stop une musique joué
 def stop():
+    global stoper
     # stop la musique
     pygame.mixer.music.stop()
     # supprime la selection de la musique stopé dans la listbox
     fenetre_liste_musique.select_clear(ACTIVE)
+    temps_progression.configure(text='')
+
+    # reset de la barre de progression
+    barre_progression.config(value=0)
+    temps_progression.config(text='')
+
+    # set stop varaible to true
+    stoper = True
 
 
 # fonction qui met en pause une musique
@@ -91,6 +111,8 @@ def pause():
 
 # focntion musique suivante
 def musique_suivante():
+    barre_progression.config(value=0)
+    temps_progression.config(text='')
     # avoir la position dans la liste de la musique actuelle
     suivante = fenetre_liste_musique.curselection()
     # on ajoute +1 a la position de la muisque actuelle
@@ -107,6 +129,8 @@ def musique_suivante():
 
 # fonction musique précédente
 def musique_precedente():
+    barre_progression.config(value=0)
+    temps_progression.config(text='')
     # avoir la position dans la liste de la musique actuelle
     suivante = fenetre_liste_musique.curselection()
     # on soustrait - 1 a la porsiton actuelle
@@ -121,20 +145,73 @@ def musique_precedente():
     lancer_musique()
 
 
-# FONCTION BARRE DE PROGRESSIOB
+# FONCTION TEMPS DE PROGRESSION
 def temps_musique():
-    temps_actuelle = pygame.mixer.music.get_pos() / 1000
+    global musique_longeur
+    global en_pause
+    global stoper
 
-    temps_actuelle_modif = time.strftime('%M:%S', time.gmtime(temps_actuelle))
-    barre_progression.configure(text=temps_actuelle_modif)
-    barre_progression.after(1000, temps_musique)
+    if stoper == True:
+        return
+
+    temps_actuelle = pygame.mixer.music.get_pos() / 1000
+    # label date
+    slider_label.config(text=f'slider: {int(barre_progression.get())} and posditon:{int(temps_actuelle)}')
+    #temps_actuelle_modif = time.strftime('%M:%S', time.gmtime(temps_actuelle))
+    # avoir la longueur du temps
+
+    # musique_actuelle_temps = fenetre_liste_musique.curselection()
+    titre_musique = fenetre_liste_musique.get(ACTIVE)
+    tire_musique = f'/Users/raphaelmalet/Documents/git/playerMusic/musique/{titre_musique}.mp3'
+    musique_mut = MP3(tire_musique)
+    musique_longeur = musique_mut.info.length
+    convertir_musique_longeur = time.strftime('%M:%S', time.gmtime(musique_longeur))
+
+    temps_actuelle += 1
+    if int(barre_progression.get()) == int(musique_longeur):
+        temps_progression.config(text=f'{convertir_musique_longeur} / {convertir_musique_longeur}')
+        pass
+
+    elif en_pause:
+        pass
+
+    # augmente le temps actuelle de 1s
+    elif int(barre_progression.get()) == int(temps_actuelle):
+        slider_position = int(musique_longeur)
+        barre_progression.config(to=slider_position, value=int(temps_actuelle))
+
+
+    else:
+        slider_position = int(musique_longeur)
+        barre_progression.config(to=slider_position, value=int(barre_progression.get()))
+        temps_actuelle_modif = time.strftime('%M:%S', time.gmtime(barre_progression.get()))
+        temps_progression.config(text=f'{temps_actuelle_modif} / {convertir_musique_longeur}')
+        # bouger de 1 seconde
+        next_time = int(barre_progression.get())
+        barre_progression.config(to=slider_position, value=next_time)
+
+    # actualise la progression de la barre en fonction du temps d'écoute de la musique
+    # barre_progression.config(value=temps_actuelle)
+    # temps_progression.config(text=f'{temps_actuelle_modif} / {convertir_musique_longeur}')
+    # actualisation du temps de progresion
+    temps_progression.after(1000, temps_musique)
+
+
+def slider(x):
+    global musique_longeur
+    slider_label.config(text=f'{int(barre_progression.get())} sur {int(musique_longeur)}')
+    musique = fenetre_liste_musique.get(ACTIVE)
+    musique = f'/Users/raphaelmalet/Documents/git/playerMusic/musique/{musique}.mp3'
+
+    pygame.mixer.music.load(musique)
+    pygame.mixer.music.play(loops=0, start=int(barre_progression.get()))
 
 
 if __name__ == '__main__':
-    fenetre = Tk()  # nom de la fenetre
-    fenetre.configure(bg='')  # background de la fenetre
-    fenetre.geometry('500x700')  # taille de la fenetre
-    fenetre.title('PlayerMusic')  # titre de la fenetre
+    playermusic = Tk()  # nom de la fenetre
+    playermusic.configure(bg='')  # background de la fenetre
+    playermusic.geometry('500x700')  # taille de la fenetre
+    playermusic.title('PlayerMusic')  # titre de la fenetre
 
     # permet a pygame de lancer les musiques
     pygame.mixer.init()
@@ -143,15 +220,15 @@ if __name__ == '__main__':
     # MENU
     # ________
     # creation menu
-    menu_choix = Menu(fenetre)
-    fenetre.configure(menu=menu_choix)
+    menu_choix = Menu(playermusic)
+    playermusic.configure(menu=menu_choix)
 
     # ajout musique au player
     ajout_musique = Menu(menu_choix)
     menu_choix.add_cascade(label="ajouter musique", menu=ajout_musique)
     # ajouter une musique au player
     ajout_musique.add_command(label='ajouter une musique', command=ajout_une_musique)
-    # ajout plusieurs musique au player
+    # ajout plusieurs musiques au player
     ajout_musique.add_command(label='ajouter plusieur musique', command=ajout_plusieur_musique)
 
     supprime_musique = Menu(menu_choix)
@@ -170,15 +247,15 @@ if __name__ == '__main__':
     precedent_img = PhotoImage(file="img/piste-precedente_50.png")
 
     # fenetre liste des musiques
-    fenetre_liste_musique = Listbox(fenetre, height=10, width=50, selectbackground='grey')
+    fenetre_liste_musique = Listbox(playermusic, height=10, width=50, selectbackground='grey')
     fenetre_liste_musique.pack(pady=20)
 
-    # barre de status
-    barre_progression = Label(fenetre, text='')
-    barre_progression.pack(fill=X, ipady=3)
+    # temps de la musique
+    temps_progression = Label(playermusic, text='')
+    temps_progression.pack(fill=X, ipady=3)
 
     # fenetre disposition des boutons
-    fenetre_bouton = Label(fenetre, height=5, width=50)
+    fenetre_bouton = Label(playermusic, height=5, width=50)
     fenetre_bouton.pack()
 
     # disposition des boutons de control dans la fenetre dispostion des boutons
@@ -193,4 +270,11 @@ if __name__ == '__main__':
     precedent_bouton = Button(fenetre_bouton, image=precedent_img, borderwidth=0, command=musique_precedente)
     precedent_bouton.grid(row=0, column=0, padx=10)
 
-    fenetre.mainloop()
+    # fenetre slider
+    barre_progression = ttk.Scale(playermusic, from_=0, to=100, orient=HORIZONTAL, value=0, command=slider, length=455)
+    barre_progression.pack(pady=10, )
+
+    slider_label = Label(playermusic, text='0')
+    slider_label.pack(pady=10)
+
+    playermusic.mainloop()
